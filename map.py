2cd4796ae0eabedc059ce4b043dbc961fba24988
DanielSamp4 <daniel.sampaio0144@gmail.com>
2022-12-13 18:49:50 -0300

    up

---
+++ map.py
@@ -0,0 +1,14 @@
+from cgi import test
+
+
+class map:
+    
+    street_width = 2
+    
+
+    def __init__(self, width, length):
+        self.width = width
+        self.lenght = length
+
+
+        
