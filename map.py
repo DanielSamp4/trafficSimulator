3dff0c42d7f3b5b17b7651b6f04872abcf4de443
DanielSamp4 <daniel.sampaio0144@gmail.com>
2022-12-15 00:52:06 -0300

    up

--- map.py
+++ map.py
@@ -3,12 +3,18 @@ from cgi import test
 
 class map:
     
-    street_width = 2
-    
-
     def __init__(self, width, length):
         self.width = width
         self.lenght = length
 
+class street(map):
+    street_width = 2
+    street_x,street_y = 0  
+    
 
+    def __init__(self):
+        pass
+
+    def make_street():
+        pass
         
